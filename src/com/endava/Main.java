a291423a4c09a06d23561cbcd4cf7c0fc51f4b90
vcumpana <victorc@bk.ru>
2018-03-28 22:07:24 +0300

    first 3 ex done

--- src/com/endava/Main.java
+++ src/com/endava/Main.java
@@ -22,49 +22,63 @@ public class Main {
 
     public static void main(String args[]) {
 
-        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
-                .stream()
-                .filter(num -> num % 2 == 0)
-                .forEach(System.out::println);
+//        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+//                .stream()
+//                .filter(num -> num % 2 == 0)
+//                .forEach(System.out::println);
+//
+//        printDelimiterLine();
+//
+//        USERS.stream()
+//                .parallel()
+//                .map(User::getName)
+//                .filter(string -> string.startsWith("A"))
+//                .forEach(System.out::println);
+//
+//        printDelimiterLine();
+//
+//        System.out.println(USERS
+//                .stream()
+//                .collect(Collectors.groupingBy(User::getGender)));
+//        printDelimiterLine();
+//
+//        System.out.println(Stream.of(new User("Vasea", MALE, 39),
+//                new User("Sveta", FEMALE, 30),
+//                new User("Jenea", MALE, 40))
+//                .map(User::getName)
+//                .collect(Collectors.joining(" *** ")));
+//
+//        printDelimiterLine();
+//
+//        System.out.println(Stream.of(new User("Vasea", MALE, 39),
+//                new User("Sveta", FEMALE, 30),
+//                new User("Jenea", MALE, 40))
+//                .collect(Collectors.groupingBy(User::getGender, Collectors.counting())));
+//
+//        printDelimiterLine();
+//
+//        System.out.println(
+//                USERS.stream()
+//                        .filter(user -> user.getGender() == MALE)
+//                        .sorted(Comparator.comparing(User::getAge))
+//                        .collect(Collectors.toSet()));
+//
+//        printDelimiterLine();
+        charCounting("abcddd");
+        wordCounting(" 1 2 3 4 5 1 1 3 5 ");
+        System.out.println(filteredUsersByAge(USERS));
+    }
 
-        printDelimiterLine();
+    public static void charCounting(String s){
+        System.out.println(s.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(c -> c, Collectors.counting())));
+    }
 
-        USERS.stream()
-                .parallel()
-                .map(User::getName)
-                .filter(string -> string.startsWith("A"))
-                .forEach(System.out::println);
+    public static void wordCounting(String s){
+        System.out.println(Arrays.stream(s.trim().split(" ")).collect(Collectors.groupingBy(c -> c, Collectors.counting())));
+    }
 
-        printDelimiterLine();
-
-        System.out.println(USERS
-                .stream()
-                .collect(Collectors.groupingBy(User::getGender)));
-        printDelimiterLine();
-
-        System.out.println(Stream.of(new User("Vasea", MALE, 39),
-                new User("Sveta", FEMALE, 30),
-                new User("Jenea", MALE, 40))
-                .map(User::getName)
-                .collect(Collectors.joining(" *** ")));
-
-        printDelimiterLine();
-
-        System.out.println(Stream.of(new User("Vasea", MALE, 39),
-                new User("Sveta", FEMALE, 30),
-                new User("Jenea", MALE, 40))
-                .collect(Collectors.groupingBy(User::getGender, Collectors.counting())));
-
-        printDelimiterLine();
-
-        System.out.println(
-                USERS.stream()
-                        .filter(user -> user.getGender() == MALE)
-                        .sorted(Comparator.comparing(User::getAge))
-                        .collect(Collectors.toSet()));
-
-        printDelimiterLine();
-
+    public static List<User> filteredUsersByAge(List<User> users){
+        return users.stream().filter(user -> user.getAge() > 40).collect(Collectors.toList());
     }
 
     public static void printDelimiterLine() {
